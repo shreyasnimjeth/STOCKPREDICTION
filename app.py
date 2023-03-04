ef19900389c0964c6f0bbfac68e64690e071ab83
shreyasnimjeth <75248026+shreyasnimjeth@users.noreply.github.com>
2023-03-05 03:08:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import prediction,new, about# import your app modules here
+from apps import new, about# import your app modules here
 
 app = MultiApp()
 
@@ -12,7 +12,7 @@ st.markdown("""
 # Add all your application here
 
 app.add_app("HOME", new.app)
-app.add_app("PREDITION", prediction.app)
+#app.add_app("PREDITION", prediction.app)
 
 app.add_app("ABOUT", about.app)
 
